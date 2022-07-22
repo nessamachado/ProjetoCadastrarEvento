4c5d8cce71cdbe0211c00bce971150b0b8a0dd5a
Vanessa Alves de Souza Machado <107202228+nessamachado@users.noreply.github.com>
2022-07-22 11:27:39 -0300

    eventos
    Cadastro de eventos

---
+++ eventoapp/src/main/java/com/eventoapp/controllers/IndexContoller.java
@@ -0,0 +1,14 @@
+package com.eventoapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexContoller {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+}
